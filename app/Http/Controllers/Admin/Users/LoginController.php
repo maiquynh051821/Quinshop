a4c70b2962d7a212e9ea2daf3af0cf3307e39455
maiquynh051821 <102955491+maiquynh051821@users.noreply.github.com>
2024-04-09 10:17:10 +0700

    Admin login 1

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+    public function index()
+    {
+        return view("admin.users.login", [
+            "title" => "Login Quin-Shop",
+        ]);
+    }
+    public function store(Request $request)
+    {
+       $this->validate($request, [
+        "email"=> "required|email:filter",
+        "password"=> "required"
+       ]);
+    }
+}
